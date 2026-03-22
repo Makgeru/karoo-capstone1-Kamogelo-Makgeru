3c2d31656f1a86cf670213189345680a4a0fa469
Makgeru <your-id+username@users.noreply.github.com>
2026-03-22 21:16:35 +0200

    creating tables

---
+++ INSERT INTO orders (order_id, supplier_i.sql
@@ -0,0 +1,7 @@
+INSERT INTO orders (order_id, supplier_id, order_date, total_price)
+VALUES (
+    'order_id:bigint',
+    'supplier_id:bigint',
+    'order_date:text',
+    'total_price:bigint'
+  );
