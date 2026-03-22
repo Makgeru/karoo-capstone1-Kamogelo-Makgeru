b25f76b51c763ac9000dac26781d0f800c8dbcbe
Makgeru <your-id+username@users.noreply.github.com>
2026-03-22 21:19:10 +0200

    removing wrong file

--- INSERT INTO orders (order_id, supplier_i.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO orders (order_id, supplier_id, order_date, total_price)
-VALUES (
-    'order_id:bigint',
-    'supplier_id:bigint',
-    'order_date:text',
-    'total_price:bigint'
-  );
